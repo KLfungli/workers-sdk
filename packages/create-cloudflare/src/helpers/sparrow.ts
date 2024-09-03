9713729a61696a472d35e01d7a4fed6ab6668617
Edmund Hung <edmund@cloudflare.com>
2024-09-03 17:40:14 +0100

    improve sparrow source key handling

--- packages/create-cloudflare/src/helpers/sparrow.ts
+++ packages/create-cloudflare/src/helpers/sparrow.ts
@@ -1,9 +1,8 @@
 import { fetch } from "undici";
 
-// The SPARROW_SOURCE_KEY is provided at esbuild time as a `define` for production and beta
-// releases. Otherwise it is left undefined, which automatically disables metrics requests.
-declare const SPARROW_SOURCE_KEY: string;
-const SPARROW_URL = "https://sparrow.cloudflare.com";
+// The __SPARROW_SOURCE_KEY__ will be replaced with the env value at build time through esbuild's `define` option
+const SPARROW_SOURCE_KEY: string = "__SPARROW_SOURCE_KEY__";
+const SPARROW_URL: string = "https://sparrow.cloudflare.com";
 
 export type EventPayload = {
 	event: string;
@@ -14,21 +13,16 @@ export type EventPayload = {
 };
 
 export function hasSparrowSourceKey() {
-	return SPARROW_SOURCE_KEY !== "";
+	return SPARROW_SOURCE_KEY !== "__SPARROW_SOURCE_KEY__";
 }
 
 export async function sendEvent(payload: EventPayload) {
-	if (!SPARROW_SOURCE_KEY) {
-		return;
-	}
-
 	await fetch(`${SPARROW_URL}/api/v1/event`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 			"Sparrow-Source-Key": SPARROW_SOURCE_KEY,
 		},
-		keepalive: true,
 		body: JSON.stringify(payload),
 	});
 }
